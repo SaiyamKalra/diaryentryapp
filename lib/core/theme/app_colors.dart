61bac867e23433724bbd547204cda91a0cd122d8
Saiyam Kalra <kalrasaiyam2006@gmail.com>
2025-05-05 11:46:42 +0530

    completed main page and added page for theme design

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+ThemeData darkTheme() {
+  return ThemeData.dark(useMaterial3: true).copyWith();
+}
+
+ThemeData lightTheme() {
+  return ThemeData.light(useMaterial3: true).copyWith();
+}
---
+++ lib/pages/main.dart
@@ -0,0 +1,28 @@
+import 'package:diaryentryapp/cubit/diary_cubit.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../core/theme/app_colors.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SharedPreferences.getInstance();
+  runApp(
+    BlocProvider(create: (context) => DiaryCubit(), child: const MainApp()),
+  );
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.system,
+      theme: lightTheme(),
+      darkTheme: darkTheme(),
+    );
+  }
+}
