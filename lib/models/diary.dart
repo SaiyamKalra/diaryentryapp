6f99207f71f549bc1b6056ea2244e71b399a6588
Saiyam Kalra <kalrasaiyam2006@gmail.com>
2025-05-05 11:23:24 +0530

    added cubit to the app

--- lib/models/diary.dart
+++ lib/models/diary.dart
@@ -2,11 +2,12 @@ class Diary {
   final String title;
   final String body;
   final DateTime date;
+  final double star;
 
-  Diary({required this.title, required this.body, required this.date});
+  Diary({required this.title, required this.body, required this.date,required this.star});
 
   Map<String, dynamic> toJson() {
-    return {'title': title, 'body': body, 'date': date.toIso8601String()};
+    return {'title': title, 'body': body, 'date': date.toIso8601String(),'star':star};
   }
 
   factory Diary.fromJson(Map<String, dynamic> json) {
@@ -14,6 +15,7 @@ class Diary {
       title: json['title'],
       body: json['body'],
       date: DateTime.parse(json['date']),
+      star: json['star'],
     );
   }
 }
