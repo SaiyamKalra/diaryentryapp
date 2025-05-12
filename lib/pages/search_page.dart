7645de14b9704528493a3a42b101e9bdfadacab0
Saiyam Kalra <kalrasaiyam2006@gmail.com>
2025-05-12 19:43:58 +0530

    completed the happy mood page

--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -2,9 +2,28 @@ import 'package:flutter/material.dart';
 
 class SearchPage extends StatelessWidget {
   const SearchPage({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final TextEditingController searchText = TextEditingController();
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/search.png"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: TextField(
+          controller: searchText,
+          decoration: InputDecoration(
+            hintText: 'Search Diary',
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(30.0),
+            ),
+            prefixIcon: Icon(Icons.search),
+          ),
+        ),
+      ),
+    );
   }
 }
