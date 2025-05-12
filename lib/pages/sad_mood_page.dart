b449c4d511eaccaaf780c139737228a964603ee6
Saiyam Kalra <kalrasaiyam2006@gmail.com>
2025-05-12 20:07:35 +0530

    completed the sad_mood_page

--- lib/pages/sad_mood_page.dart
+++ lib/pages/sad_mood_page.dart
@@ -1,10 +1,51 @@
+import 'package:diaryentryapp/models/diary.dart';
+import 'package:diaryentryapp/utils/diary_card.dart';
 import 'package:flutter/material.dart';
+import 'package:diaryentryapp/cubit/diary_cubit.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SadMoodPage extends StatelessWidget {
   const SadMoodPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/background.png"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: BlocBuilder<DiaryCubit, List<Diary>>(
+          builder: (context, diaries) {
+            final filteredDiaries =
+                diaries.where((diary) {
+                  final rating = double.tryParse(diary.star) ?? 0.0;
+                  return rating <= 3.5;
+                }).toList();
+            if (filteredDiaries.isEmpty) {
+              return Center(
+                child: Text(
+                  'Nothing Bad Shared With Us!!',
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                ),
+              );
+            }
+            return ListView.builder(
+              itemCount: filteredDiaries.length,
+              itemBuilder: (context, index) {
+                final diary = filteredDiaries[index];
+                return DiaryCard(
+                  title: diary.title,
+                  number: diary.number,
+                  star: diary.star,
+                );
+              },
+            );
+          },
+        ),
+      ),
+    );
   }
 }
